272c8550cd8fa4798031204b64dac0a5d747714c
Jason Wang <jwang619@gmail.com>
2023-03-02 12:25:26 -0500

    prepare to address this USERNAME issue, and then we will split off and do our views/components.

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
-const { db } = require('./db')
-const PORT = process.env.PORT || 8080
-const app = require('./app')
-const seed = require('../script/seed');
+const { db } = require("./db");
+const PORT = process.env.PORT || 8080;
+const app = require("./app");
+//const seed = require("../script/seed");
+// WARNING!! A file requirement will RUN the contents of that file!
+// If that file has a line such as "functionDX()", then functionDX will run when the file is required.
 
 const init = async () => {
   try {
-    if(process.env.SEED === 'true'){
-      await seed();
-    }
-    else {
-      await db.sync()
-    }
+    await db.sync();
     // start listening (and create a 'server' object representing our server)
-    app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
+    app.listen(PORT, () =>
+      console.log(
+        `Mixing it up on port ${PORT}!!!!!!!!!!!!! server --> index.js, line 11`
+      )
+    );
   } catch (ex) {
-    console.log(ex)
+    console.log(ex);
   }
-}
+};
 
-init()
+init();
