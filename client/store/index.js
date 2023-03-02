ab5262f53ee3c5b4d15ca9494401247d6cd7b5a6
Forhad Zinnah <forhad@Forhads-Mac-mini.local>
2023-03-02 15:41:31 -0800

    new start for frontend

--- client/store/index.js
+++ client/store/index.js
@@ -1,12 +1,27 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
+// import { configureStore } from '@reduxjs/toolkit'
+// import cartSlice from './reducers/cartSlice'
+// import productsSlice from './reducers/productsSlice'
+// import userSlice from './reducers/userSlice'
+
+// const store = configureStore({
+//   reducer: {
+//     // cart: cartSlice,
+//     // user: userSlice,
+//     // products: productsSlice,
+//   },
+// })
+
+// export default store
+
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import auth from './auth'
 
 const reducer = combineReducers({ auth })
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 )
 const store = createStore(reducer, middleware)
 
