84a6419bde3c9bd4ff5393080564c30c12d88456
kbwong3 <brianwong0109@gmail.com>
2023-03-07 23:57:53 -0500

    styling stuff

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { NavLink } from 'react-router-dom'
-import { logout } from '../store'
-import NavCart from './NavCart'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import Container from 'react-bootstrap/Container'
-import Button from 'react-bootstrap/Button'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { logout } from "../store";
+import NavCart from "./NavCart";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
 const NavBar = () => {
-  const isLoggedIn = useSelector((state) => !!state.auth.id)
-  const dispatch = useDispatch()
+  const isLoggedIn = useSelector((state) => !!state.auth.id);
+  const dispatch = useDispatch();
 
   return (
     <Navbar sticky="top" id="navbar" className="shadow-sm mb-3">
@@ -35,12 +35,14 @@ const NavBar = () => {
             <Button href="#" onClick={() => dispatch(logout())}>
               Log Out
             </Button>
-            <NavCart />
+            <div className="mr-auto">
+              <NavCart style="float:right" />
+            </div>
           </Nav>
         </Container>
       ) : (
         <Container>
-          <Navbar.Brand>Dactyl Arboretum</Navbar.Brand>
+          <Navbar.Brand>Dactyl Arboretum🌱🌱</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link to="/" as={NavLink}>
               Home
@@ -62,7 +64,7 @@ const NavBar = () => {
         </Container>
       )}
     </Navbar>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
