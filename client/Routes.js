c9d1e3d4057378d37076d53fbc157b689ed12397
Forhad Zinnah <forhad@Forhads-Mac-mini.local>
2023-03-07 17:10:46 -0800

    removed add to cart buttons/cart icon for users not logged in

--- client/Routes.js
+++ client/Routes.js
@@ -1,26 +1,26 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import { Login, Signup } from "./components/AuthForm";
-import UserDashboard from "./components/UserDashboard";
-import { me } from "./store";
-import UserProfile from "./components/UserProfile";
-import AllProducts from "./components/AllProducts";
-import SingleProduct from "./components/SingleProduct";
-import OrderSummary from "./components/OrderSummary";
-import Checkout from "./components/Checkout";
-import CartItems from "./components/CartItems";
-import PageNotFound from "./components/PageNotFound";
-import About from "./components/About";
-import Careers from "./components/Careers";
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Route, Switch } from 'react-router-dom'
+import { Login, Signup } from './components/AuthForm'
+import UserDashboard from './components/UserDashboard'
+import { me } from './store'
+import UserProfile from './components/UserProfile'
+import AllProducts from './components/AllProducts'
+import SingleProduct from './components/SingleProduct'
+import OrderSummary from './components/OrderSummary'
+import Checkout from './components/Checkout'
+import CartItems from './components/CartItems'
+import PageNotFound from './components/PageNotFound'
+import About from './components/About'
+import Careers from './components/Careers'
 
 const Routes = () => {
-  const isLoggedIn = useSelector((state) => !!state.auth.id);
-  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => !!state.auth.id)
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(me());
-  }, []);
+    dispatch(me())
+  }, [])
 
   return (
     <div>
@@ -40,28 +40,28 @@ const Routes = () => {
         </Switch>
       ) : (
         <Switch>
-          <Route exact path="/products" component={AllProducts} />
+          {/* <Route exact path="/products" component={AllProducts} /> */}
           <Route exact path="/about" component={About} />
           <Route exact path="/products/:id" component={SingleProduct} />
           <Route exact path="/careers" component={Careers} />
 
-          <Route exact path="/users/:id" component={UserProfile} />
-          <Route exact path="/cart" component={CartItems} />
+          {/* <Route exact path="/users/:id" component={UserProfile} /> */}
+          {/* <Route exact path="/cart" component={CartItems} /> */}
           <Route exact path="/products" component={AllProducts} />
-          <Route exact path="/checkout" component={Checkout} />
+          {/* <Route exact path="/checkout" component={Checkout} /> */}
           <Route exact path="/login">
             {Login}
           </Route>
           <Route exact path="/signup">
-            {Signup}{" "}
+            {Signup}{' '}
           </Route>
-          <Route exact path="/ordersummary" component={OrderSummary} />
+          {/* <Route exact path="/ordersummary" component={OrderSummary} /> */}
           <Route exact path="/" component={AllProducts} />
           <Route path="/*" component={PageNotFound} />
         </Switch>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Routes;
+export default Routes
--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,40 +1,41 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
 import {
   fetchAllProducts,
   selectProducts,
-} from "../store/reducers/productsSlice";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { addToCartAsync } from "../store/reducers/cartSlice";
-import { ToastContainer, toast } from "react-toastify";
-import Filter from "./Filter";
+} from '../store/reducers/productsSlice'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import { addToCartAsync } from '../store/reducers/cartSlice'
+import { ToastContainer, toast } from 'react-toastify'
+import Filter from './Filter'
 
 function AllProducts() {
-  const dispatch = useDispatch();
-  const products = useSelector(selectProducts);
-  const id = useSelector((state) => state.auth.id);
+  const dispatch = useDispatch()
+  const products = useSelector(selectProducts)
+  const id = useSelector((state) => state.auth.id)
+  const isLoggedIn = useSelector((state) => !!state.auth.id)
 
   const notify = (name) => {
     toast.success(`Added ${name} to your cart.`, {
-      position: "bottom-left",
+      position: 'bottom-left',
       autoClose: 2000,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
       draggable: true,
       progress: undefined,
-      theme: "colored",
-    });
-  };
+      theme: 'colored',
+    })
+  }
 
   useEffect(() => {
-    dispatch(fetchAllProducts());
-  }, [dispatch]);
+    dispatch(fetchAllProducts())
+  }, [dispatch])
 
   return (
     <>
@@ -44,7 +45,7 @@ function AllProducts() {
         <Row>
           {products.map((product) => (
             <Col xs={3} className="mb-5" key={product.id}>
-              <Card className="all-prod" style={{ width: "18rem" }}>
+              <Card className="all-prod" style={{ width: '18rem' }}>
                 <Link
                   to={`/products/${product.id}`}
                   className="stretched-link"
@@ -56,31 +57,34 @@ function AllProducts() {
                   <div>Price: ${product.price}</div>
                 </Card.Body>
               </Card>
-
-              <Button
-                className="mt-3"
-                variant="info"
-                onClick={() => {
-                  dispatch(
-                    addToCartAsync({
-                      productId: product.id,
-                      price: product.price,
-                      qty: 1,
-                      id: id,
-                    })
-                  );
-                  notify(product.name);
-                }}
-              >
-                Add to Cart
-              </Button>
+              {isLoggedIn ? (
+                <Button
+                  className="mt-3"
+                  variant="info"
+                  onClick={() => {
+                    dispatch(
+                      addToCartAsync({
+                        productId: product.id,
+                        price: product.price,
+                        qty: 1,
+                        id: id,
+                      })
+                    )
+                    notify(product.name)
+                  }}
+                >
+                  Add to Cart
+                </Button>
+              ) : (
+                <></>
+              )}
             </Col>
           ))}
         </Row>
       </Container>
       <ToastContainer />
     </>
-  );
+  )
 }
 
-export default AllProducts;
+export default AllProducts
