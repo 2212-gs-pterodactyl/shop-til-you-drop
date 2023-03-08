b59e06161986f31513d2eda3707269c38f611e13
kbwong3 <brianwong0109@gmail.com>
2023-03-07 23:48:58 -0500

    quick styling touchup

--- client/components/UserDashboard.js
+++ client/components/UserDashboard.js
@@ -1,23 +1,29 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import AdminProfile from './AdminProfile'
-import UserProfile from './UserProfile'
+import React from "react";
+import { useSelector } from "react-redux";
+import AdminProfile from "./AdminProfile";
+import UserProfile from "./UserProfile";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
 
 const UserDashboard = (props) => {
-  const email = useSelector((state) => state.auth.email)
-  const { firstName, role } = useSelector((state) => state.auth)
+  const email = useSelector((state) => state.auth.email);
+  const { firstName, role } = useSelector((state) => state.auth);
 
   return (
-    <div>
-      <h3>
-        Welcome
-        {firstName
-          ? ', ' + firstName + '!'
-          : '! Use this page to log your information.'}
-      </h3>
-      {role === 'admin' ? <AdminProfile /> : <UserProfile />}
-    </div>
-  )
-}
+    <Container>
+      <Col className="ml-4">
+        <div>
+          <h3>
+            Welcome
+            {firstName
+              ? ", " + firstName + "!"
+              : "! Use this page to log your information."}
+          </h3>
+          {role === "admin" ? <AdminProfile /> : <UserProfile />}
+        </div>
+      </Col>
+    </Container>
+  );
+};
 
-export default UserDashboard
+export default UserDashboard;
